6f6669e159ebf0e33a2ac20d4419060635d368df
otech2018 <ajahogonnaya2012@gmail.com>
2021-05-07 15:27:30 +0100

    desktop header complete

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,368 @@
+<div id="page-footer-wrapper" class="page-footer-wrapper">
+	<div class="page-footer elementor-footer" id="page-footer">
+			<div data-elementor-type="footer" data-elementor-id="541" class="elementor elementor-541 elementor-location-footer" data-elementor-settings="[]">
+		<div class="elementor-section-wrap">
+					<section class="elementor-section elementor-top-section elementor-element elementor-element-77fe815 elementor-section-boxed elementor-section-gap-beside-yes elementor-section-height-default elementor-section-height-default elementor-section-column-vertical-align-stretch" data-id="77fe815" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-extended">
+							<div class="elementor-row">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-a3b9c20" data-id="a3b9c20" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<section class="elementor-section elementor-inner-section elementor-element elementor-element-6110216 elementor-section-gap-beside-no elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section-column-vertical-align-stretch" data-id="6110216" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-extended">
+							<div class="elementor-row">
+					<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-d544228" data-id="d544228" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-c487d5a elementor-widget elementor-widget-tm-heading" data-id="c487d5a" data-element_type="widget" data-widget_type="tm-heading.default">
+				<div class="elementor-widget-container">
+					<div class="tm-modern-heading">
+			
+					<div class="heading-primary-wrap">
+			<h3 class="heading-primary elementor-heading-title">About</h3>		</div>
+		
+			
+					</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-ea330df edumall-list-layout-block elementor-widget elementor-widget-tm-list" data-id="ea330df" data-element_type="widget" data-widget_type="tm-list.default">
+				<div class="elementor-widget-container">
+					<div class="edumall-list">
+								<div class="item elementor-repeater-item-03d9b22">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">About Us</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-6dd1bdb">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Courses</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-7e36fa2">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Instructor</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-e966fdf">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Events</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-197fb31">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Become A Teacher</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+							</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-2674061 elementor-widget elementor-widget-spacer" data-id="2674061" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-51cea53" data-id="51cea53" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-ca52b57 elementor-widget elementor-widget-tm-heading" data-id="ca52b57" data-element_type="widget" data-widget_type="tm-heading.default">
+				<div class="elementor-widget-container">
+					<div class="tm-modern-heading">
+			
+					<div class="heading-primary-wrap">
+			<h3 class="heading-primary elementor-heading-title">Links</h3>		</div>
+		
+			
+					</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-2ab8416 edumall-list-layout-block elementor-widget elementor-widget-tm-list" data-id="2ab8416" data-element_type="widget" data-widget_type="tm-list.default">
+				<div class="elementor-widget-container">
+					<div class="edumall-list">
+								<div class="item elementor-repeater-item-03d9b22">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">News &amp; Blogs</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-6dd1bdb">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Library</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-7e36fa2">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Gallery</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-e966fdf">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Partners</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-197fb31">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Career</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+							</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-a85a249 elementor-widget elementor-widget-spacer" data-id="a85a249" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-8ec4361" data-id="8ec4361" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-25fd8c0 elementor-widget elementor-widget-tm-heading" data-id="25fd8c0" data-element_type="widget" data-widget_type="tm-heading.default">
+				<div class="elementor-widget-container">
+					<div class="tm-modern-heading">
+			
+					<div class="heading-primary-wrap">
+			<h3 class="heading-primary elementor-heading-title">Support</h3>		</div>
+		
+			
+					</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-673a639 edumall-list-layout-block elementor-widget elementor-widget-tm-list" data-id="673a639" data-element_type="widget" data-widget_type="tm-list.default">
+				<div class="elementor-widget-container">
+					<div class="edumall-list">
+								<div class="item elementor-repeater-item-03d9b22">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Documentation</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-6dd1bdb">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">FAQs</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-7e36fa2">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Forum</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-e966fdf">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Sitemap</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+							</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-6c21fa5 elementor-widget elementor-widget-spacer" data-id="6c21fa5" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-6992555" data-id="6992555" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-3e917c6 elementor-widget elementor-widget-image" data-id="3e917c6" data-element_type="widget" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-image">
+											<a href="index.html">
+							<img width="296" height="64" src="wp-content/uploads/2020/08/dark-logo.png" class="attachment-full size-full" alt="" loading="lazy" />								</a>
+											</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-5546e1c elementor-widget elementor-widget-tm-social-networks" data-id="5546e1c" data-element_type="widget" data-widget_type="tm-social-networks.default">
+				<div class="elementor-widget-container">
+					<div class="tm-social-networks style-large-icons layout-inline">
+			<ul class="list">				<li class="item elementor-repeater-item-6c90bd3">
+					<a class="link" aria-label="Twitter" href="#" target="_blank" rel="nofollow">
+						<i class="link-icon fab fa-twitter"></i>					</a>
+				</li>
+								<li class="item elementor-repeater-item-f43d121">
+					<a class="link" aria-label="Facebook" href="#" target="_blank" rel="nofollow">
+						<i class="link-icon fab fa-facebook-f"></i>					</a>
+				</li>
+								<li class="item elementor-repeater-item-2d60ce0">
+					<a class="link" aria-label="Linkedin" href="#" target="_blank" rel="nofollow">
+						<i class="link-icon fab fa-linkedin-in"></i>					</a>
+				</li>
+								<li class="item elementor-repeater-item-42687fb">
+					<a class="link" aria-label="Youtube" href="#" target="_blank" rel="nofollow">
+						<i class="link-icon fab fa-youtube"></i>					</a>
+				</li>
+				</ul>		</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-d63d5ce elementor-widget elementor-widget-tm-heading" data-id="d63d5ce" data-element_type="widget" data-widget_type="tm-heading.default">
+				<div class="elementor-widget-container">
+					<div class="tm-modern-heading">
+			
+					<div class="heading-primary-wrap">
+			<p class="heading-primary elementor-heading-title">© 2020 EduMall. All Rights Reserved</p>		</div>
+		
+			
+					</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-d31e394 edumall-list-layout-inline elementor-widget elementor-widget-tm-list" data-id="d31e394" data-element_type="widget" data-widget_type="tm-list.default">
+				<div class="elementor-widget-container">
+					<div class="edumall-list">
+								<div class="item elementor-repeater-item-03d9b22">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Terms of Use</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+										<div class="item elementor-repeater-item-6dd1bdb">
+
+						<a class="link" href="#">
+						<div class="list-header">
+																													<div class="text-wrap">
+																	<div class="text">Privacy Policy</div>
+															</div>
+
+													</div>
+
+						</a>
+					</div>
+							</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-c31eea0 elementor-widget elementor-widget-spacer" data-id="c31eea0" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+								</div>
+					</div>
+		</section>
+						</div>
+					</div>
+		</div>
+								</div>
+					</div>
+		</section>
+				</div>
+		</div>
+		</div>
+</div>
